0e5bc14ce7622b10599b21ae48d44eaafb126dc4
phil-krull <philip.krull@hotmail.com>
2016-11-21 09:14:08 -0800

    Updated views.py file

--- apps/collab/views.py
+++ apps/collab/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 	context = {
-		"message": "I like collaborating!",
+		"message": "I like collaborating, with collaborators!",
 	}
 	return render(request, "collab/index.html", context)
