bfce2118ea4831c614f333f2d36e8eb7c8da355d
Jack Brounstein <Jack.Brounstein@yahoo.com>
2016-11-21 09:16:20 -0800

    changes message

--- apps/collab/views.py
+++ apps/collab/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 	context = {
-		"message": "I like collaborating!",
+		"message": "I hope don't get a merge conflict!",
+		"name": "Jack",
 	}
 	return render(request, "collab/index.html", context)
